b153711769d31eab2f136b0822313a55675cb614
Nidan23 <dev.sulkowski@gmail.com>
2022-07-06 18:12:15 +0200

    [#2][IMPLEMENTATION] Implemented new services dependency injection

--- wwg-backend/src/main.ts
+++ wwg-backend/src/main.ts
@@ -6,20 +6,24 @@ import {VariableService} from "./service/variable.service";
 import {TodoService} from "./service/todo.service";
 import {ValidationService} from "./service/validation.service";
 import {ResponseGeneratorService} from "./service/responseGenerator.service";
+import {LoggingService} from "./service/logging.service";
 
 dotenv.config({path:`${__dirname}/assets/env/.env`})
 
 const variableService: VariableService = new VariableService()
 const responseGeneratorService: ResponseGeneratorService = new ResponseGeneratorService()
 const validationService: ValidationService = new ValidationService()
-const databaseService: DatabaseService = new DatabaseService(variableService)
+const loggingService: LoggingService = new LoggingService(variableService)
+const databaseService: DatabaseService = new DatabaseService(variableService, loggingService)
 const todoService: TodoService = new TodoService(databaseService, responseGeneratorService, variableService)
 
 const app = new App(
     [
-        new TodoController(variableService, validationService, todoService, responseGeneratorService)
+        new TodoController(variableService, validationService, todoService, responseGeneratorService, loggingService)
     ],
-    parseInt(process.env.SERVER_PORT || variableService.variables.defaultPort)
+    parseInt(process.env.SERVER_PORT || variableService.variables.defaultPort),
+    variableService,
+    loggingService
 )
 
 app.listen()
