accaad12e16b0db23fa1b5f8d2da3de5b28ad59b
Nidan23 <dev.sulkowski@gmail.com>
2022-07-06 14:23:46 +0200

    [#1][ADD] Added types of resp msg

---
+++ wwg-backend/src/type/responseMessages.type.ts
@@ -0,0 +1,14 @@
+export interface ResponseMessagesType{
+    todoAddPositive: string
+    todoAddNegative: string
+    todoDeletePositive: string
+    todoDeleteNegative: string
+    todoUpdatePositive: string
+    todoUpdateNegative: string
+    todoGetPositive: string
+    todoGetNegative: string
+    todoGetAllPositive: string
+    todoGetAllNegative: string
+    badRequest: string,
+    internalServerError: string
+}
