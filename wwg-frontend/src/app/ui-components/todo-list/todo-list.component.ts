9bb0f1783abccf8d69f1b5fbf106cf110a7de4fd
Nidan23 <dev.sulkowski@gmail.com>
2022-07-06 20:08:34 +0200

    [#8][ADD] Added todo list component

---
+++ wwg-frontend/src/app/ui-components/todo-list/todo-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {TodoService} from "../../service/todo.service";
+import {TodoModel} from "../../model/todo.model";
+import {Observable, takeUntil} from "rxjs";
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit{
+  items?: TodoModel[]
+
+  constructor(
+    private todoService: TodoService
+  ) {
+    todoService.getAllTodos()
+      .subscribe(data => {
+        this.items = data.responseData
+      })
+  }
+
+  ngOnInit() {
+  }
+
+  updateTodo(todo?: TodoModel){
+    if(todo != null)
+      this.todoService.updateTodo(todo)
+  }
+
+  removeTodo(id?: number){
+    if(id != null)
+      this.todoService.removeTodo(id)
+  }
+}
